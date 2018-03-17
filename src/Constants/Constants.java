6b62d7ee8468aff710911fa26246cf80c75b540c
giuseppe <jph94bambu@hotmail.it>
2018-03-17 10:42:08 +0100

    commit 2(db funzionante)

--- src/Constants/Constants.java
+++ src/Constants/Constants.java
@@ -3,15 +3,11 @@ package Constants;
 public class Constants {
     public static String strLogin = "SELECT * FROM users WHERE username = '%s' AND password = '%s';";
 
-    /*TODO inserisci dati db*/
+
     //public static String DB_URL = "jdbc:posgresql://localhost:5432/dbserver";
     public static String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     public static String DB_USER = "postgres";
     public static String DB_PASS = "postgres";
 
-    /*
-    String DB_USER = "root";
-    String DB_PASS = "1234";
-    String DB_URL  = "jdbc:mysql://localhost:3306/db1";
-     */
+
 }
