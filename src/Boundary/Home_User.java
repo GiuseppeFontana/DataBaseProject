2037048046036e603787c33bfbe39d3656835333
giuseppe <jph94bambu@hotmail.it>
2018-03-21 17:28:15 +0100

    1

---
+++ src/Boundary/Home_User.java
@@ -0,0 +1,23 @@
+package Boundary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Home_User {
+    public Label HomeNameLabel;
+    public Label HomeSurnameLabel;
+    public Label HomeEmailLabel;
+    public Label HomeTitleLabel;
+    @FXML
+    private Button HomeBtnLogOut;
+
+    public void LogoutMethod(ActionEvent actionEvent) {
+
+    }
+
+    public void start() {
+
+    }
+}
