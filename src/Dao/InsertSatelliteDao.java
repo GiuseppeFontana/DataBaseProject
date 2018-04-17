9d6f8c17768dc715892303e4b0ade1ae897193df
Alessia <ei.em@outlook.it>
2018-04-17 17:24:25 +0200

    Inserimento nuovo satellite

---
+++ src/Dao/InsertSatelliteDao.java
@@ -0,0 +1,73 @@
+package Dao;
+
+import Utils.Credenziali;
+import Utils.Strings;
+import javafx.scene.control.DatePicker;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+public class InsertSatelliteDao {
+
+    public static boolean req33(String satellite, LocalDate beginact, LocalDate endact, String agency) {
+        // STEP 1: dichiarazioni
+        Statement stmt = null;
+        Connection conn = null;
+        try {
+            // STEP 2: loading dinamico del driver mysql
+            Class.forName("org.postgresql.Driver");
+
+            // STEP 3: apertura connessione
+            conn = DriverManager.getConnection(Credenziali.G_DB_URL, Credenziali.G_DB_USER, Credenziali.G_DB_PASS);
+
+
+            // STEP 4: creazione ed esecuzione della query
+            stmt = conn.createStatement();
+            String sql = String.format(Strings.strInsertSatellite,satellite, beginact, endact, agency);
+            System.out.println("query:\n"+sql);
+
+            /*
+            se non metto questo la seguente riga mi da l'errore:
+            org.postgresql.util.PSQLException: L'operazione richiete un «ResultSet» scorribile mentre questo è «FORWARD_ONLY».
+             */
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            int rs = stmt.executeUpdate(sql);
+
+            if (rs != 1) {
+                System.out.println("Errore nella Query.");
+                return false;
+            }
+
+            // STEP 6: Clean-up dell'ambiente
+            stmt.close();
+            conn.close();
+
+            System.out.println("Registrazione effettuata con successo");
+            return true;
+
+        } catch (SQLException se) {
+            // Errore durante l'apertura della connessione
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Errore nel loading del driver
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Query fallita");
+        return false;
+    }
+
+}
+
