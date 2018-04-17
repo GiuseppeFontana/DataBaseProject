034fcbdafe02e957e6859a8b42b3f58710d3ffca
alessia <ei.em@outlook.it>
2018-04-17 18:19:04 +0200

    Inserimento nuovo strumento

---
+++ src/Boundary/InsertInstrument.java
@@ -0,0 +1,68 @@
+package Boundary;
+
+import Control.DBController;
+import Control.GraphicController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class InsertInstrument {
+
+    @FXML
+    public TextField Req34InstrumentText;
+    @FXML
+    public TextField Req34SatText;
+    @FXML
+    public TextField Req34StripText;
+    @FXML
+    public Button Req34InsertButton;
+    @FXML
+    public Button Req34BackButton;
+
+
+    public void start() throws Exception {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("insertInstrument.fxml"));
+        AnchorPane root = loader.load();
+        Scene scene = new Scene(root, 686, 649);
+
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public void backHome(ActionEvent actionEvent) throws Exception {
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+        GraphicController graphicController = new GraphicController();
+        graphicController.adminReqsPage();
+    }
+
+    public void Inserisci(ActionEvent actionEvent) throws Exception {
+        String instrument = Req34InstrumentText.getText();
+        String sat = Req34SatText.getText();
+        String strip = Req34StripText.getText();
+
+        if ((instrument.equals("") || sat.equals("") || strip.equals(""))){
+            GraphicController graphicController = new GraphicController();
+            String msg1 = "Inserire tutti i campi";
+            graphicController.alertError(msg1);
+        } else {
+
+        }
+        DBController dbController = new DBController();
+        if (dbController.inserimentoStrumento(instrument,sat,strip)) {
+            GraphicController graphicController = new GraphicController();
+            ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+            graphicController.adminReqsPage();
+            String msg2 = "Inserimento effettuato con successo.";
+            graphicController.alertError(msg2);
+        }
+    }
+}
---
+++ src/Dao/InsertInstrumentDao.java
@@ -0,0 +1,70 @@
+package Dao;
+
+import Utils.Credenziali;
+import Utils.Strings;
+
+import java.sql.*;
+
+public class InsertInstrumentDao {
+    public static boolean req34(String instrument, String sat, String strip) {
+        // STEP 1: dichiarazioni
+        Statement stmt = null;
+        Connection conn = null;
+        try {
+            // STEP 2: loading dinamico del driver mysql
+            Class.forName("org.postgresql.Driver");
+
+            // STEP 3: apertura connessione
+            conn = DriverManager.getConnection(Credenziali.G_DB_URL, Credenziali.G_DB_USER, Credenziali.G_DB_PASS);
+
+
+            // STEP 4: creazione ed esecuzione della query
+            stmt = conn.createStatement();
+            String sql = String.format(Strings.strInsertInstrument,instrument,sat,strip);
+            System.out.println("query:\n"+sql);
+
+            /*
+            se non metto questo la seguente riga mi da l'errore:
+            org.postgresql.util.PSQLException: L'operazione richiete un «ResultSet» scorribile mentre questo è «FORWARD_ONLY».
+             */
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            int rs = stmt.executeUpdate(sql);
+
+            if (rs != 1) {
+                System.out.println("Errore nella Query.");
+                return false;
+            }
+
+            // STEP 6: Clean-up dell'ambiente
+            stmt.close();
+            conn.close();
+
+            System.out.println("Registrazione effettuata con successo");
+            return true;
+
+        } catch (SQLException se) {
+            // Errore durante l'apertura della connessione
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Errore nel loading del driver
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Query fallita");
+        return false;
+    }
+
+}
+
